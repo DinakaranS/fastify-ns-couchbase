c4fd1a869d11d6a2e5de16ecd44a8387a4e9f6db
Dinakaran S <dinakaran@nobel-systems.com>
2022-11-21 09:59:37 +0530

    minor change

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const couchbase = require('couchbase');
 
 async function fastifyCouchbase(fastify, cBOptions, next) {
   try {
-    const { options = {}, settings = {} } = cBOptions;
-    const cluster = await couchbase.connect(options.url, {
+    const { url = '', options = {}, settings = {} } = cBOptions;
+    const cluster = await couchbase.connect(url, {
       ...options
     });
 
     // Sets a pre-configured profile called "wanDevelopment" to help avoid latency issues
     // when accessing Capella from a different Wide Area Network
     // or Availability Zone (e.g. your laptop).
-    if (settings.applyProfile) cluster.applyProfile('wanDevelopment');
+    if (settings.profileName) cluster.applyProfile(settings.profileName || 'wanDevelopment');
     const bucket = cluster.bucket(settings.bucketName);
-    const collection = bucket.scope(settings.scope || '_default').collection(settings.collection || '_default');
+    const collection = bucket.scope(settings.scopeName || '_default').collection(settings.collectionName || '_default');
     const cb = { bucket, collection, cluster };
     fastify
       .decorate('cb', cb)
@@ -30,4 +30,4 @@ function close(fastify, done) {
   fastify.cb.cluster.close().then(() => done()).catch(() => done());
 }
 
-module.exports = fp(fastifyCouchbase, '>=3.2.3');
+module.exports = fp(fastifyCouchbase, { fastify: '4.x' });
